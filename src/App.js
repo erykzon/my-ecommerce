94c5c23dfe38d0afd2abae6243ea8d5f8b51f959
Erykzon <jota.once6mail.com>
2022-10-16 16:27:26 -0500

    3 Agregando carpeta components + archivo vacio Navbar.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
         <div>
           <img src="https://enez76gwp29.exactdn.com/wp-content/uploads/2020/04/Ghibli_logo.png?strip=all&amp;lossy=1&amp;ssl=1" 
             className='header_logo' alt="logo" />
-            <div className='container' class="flex-wrap">
-            <div>Movies</div>
-            <div>Accesories</div>
-            <div>Clothes</div>
-            </div>
         </div>
       </header>
+      <div className='container flex justify-start text-sm'>
+            <div className='p-3 m-3'>Movies</div>
+            <div className='p-3 m-3'>Accesories</div>
+            <div className='p-3 m-3'>Clothes</div>
+            </div>
     </div>
   );
 }
