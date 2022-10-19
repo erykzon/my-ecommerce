848a48daea779b05464d00ed16e9eb6eb09027b5
Erykzon <jota.once6mail.com>
2022-10-18 21:25:48 -0500

    4 Modificando navbar para hacerla responsive

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ function App() {
   return (
     <div className="App">
       <header>
-        <div>
+        <section className='navbar flex sm:justify-start sm:items-stretch'>
+          <div className='img w-1/3'>
           <img src="https://enez76gwp29.exactdn.com/wp-content/uploads/2020/04/Ghibli_logo.png?strip=all&amp;lossy=1&amp;ssl=1" 
             className='header_logo' alt="logo" />
-        </div>
-      </header>
-      <div className='container flex justify-start text-sm'>
-            <div className='p-3 m-3'>Movies</div>
-            <div className='p-3 m-3'>Accesories</div>
-            <div className='p-3 m-3'>Clothes</div>
+          </div>
+           <div className='menu flex text-sm w-2/3 items-center justify-center '>
+                  <div className='p-3 m-auto '>Movies</div>
+                  <div className='p-3 m-auto '>Accesories</div>
+                  <div className='p-3 m-auto '>Clothes</div>
+                  <div className='p-3 m-auto '>Thermos</div>
+                  <div className='p-3 m-auto '>Wallpapers</div>
             </div>
+        </section>
+      </header>
     </div>
   );
 }
